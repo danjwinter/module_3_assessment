32767e2c7a264133fa0c6f6cad37e82b2a5e64a2
Dan Winter <dan.j.winter@gmail.com>
2016-03-08 09:57:25 -0700

    create api finished

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,4 +9,18 @@ class Api::V1::ItemsController < Api::V1::ApiController
     @item = Item.find(params[:id])
     respond_with @item
   end
+
+  def destroy
+    respond_with Item.find(params[:id]).destroy
+  end
+
+  def create
+    respond_with Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :image_url)
+  end
 end
--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Api::V1::ItemsController, type: :controller do
 
       get :index, format: :json
 
-      items_response = json_response[:items]
+      items_response = json_response
       expect(items_response.count).to eq 2
       expect(items_response.first).to have_key :name
       expect(items_response.first).to have_key :description
@@ -29,12 +29,42 @@ RSpec.describe Api::V1::ItemsController, type: :controller do
                          description: "The Bluth Way",
                          image_url: "http://bakedbree.com/wp-content/uploads/2010/07/july-18-2010-64web.jpg")
       get :show, id: item.id, format: :json
-      item_response
+
       expect(json_response[:name]).to eq item.name
       expect(json_response[:description]).to eq item.description
       expect(json_response[:image_url]).to eq item.image_url
       expect(json_response).to_not have_key :created_at
       expect(json_response).to_not have_key :updated_at
+    end
+  end
+
+  describe "Delete #destroy" do
+    it "returns a 204 response when item is deleted" do
+      item = Item.create(name: "Chocolate covered banana",
+                         description: "The Bluth Way",
+                         image_url: "http://bakedbree.com/wp-content/uploads/2010/07/july-18-2010-64web.jpg")
+      delete :destroy, id: item.id, format: :json
+
+      expect(response.status).to eq 204
+      expect(Item.count).to eq 0
+    end
+  end
+
+  describe "Post #create" do
+    it "returns created item and a 201 response" do
+    # item = Item.create(name: "Chocolate covered banana",
+    #                    description: "The Bluth Way",
+    #                    image_url: "http://bakedbree.com/wp-content/uploads/2010/07/july-18-2010-64web.jpg")
+
+      post :create, format: :json, name: "Chocolate covered banana",
+                         description: "The Bluth Way",
+                         image_url: "http://bakedbree.com/wp-content/uploads/2010/07/july-18-2010-64web.jpg"
+
+     expect(json_response[:name]).to eq "Chocolate covered banana"
+     expect(json_response[:description]).to eq "The Bluth Way"
+     expect(json_response[:image_url]).to eq "http://bakedbree.com/wp-content/uploads/2010/07/july-18-2010-64web.jpg"
+     expect(json_response).to_not have_key :created_at
+     expect(json_response).to_not have_key :updated_at
     end
   end
 end
